e60b936d1e3742192857e435a34ecb1801e0b3c6
Shubham-Tiwari-Anywhere <shubham.prem@anywhere.co>
2024-01-12 17:38:59 +0530

    commented the localhost port

--- api/index.js
+++ api/index.js
@@ -12,10 +12,10 @@ app.use("/", routes);
 app.use("/query/", query);
 app.use("/post/", postReq);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
 
 module.exports = app
