c3d94a3a9127cf67dfb7479c9d4a24f003f3537d
Shubham-Tiwari-Anywhere <shubham.prem@anywhere.co>
2024-02-01 20:01:08 +0530

    commit 2 from demo2

--- routes/6-Cookies/index.js
+++ routes/6-Cookies/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const router = express.Router();
 
-console.log("Checking the commit")
+console.log("Checking the commit 2")
 
 router.use(cookieParser());
 
